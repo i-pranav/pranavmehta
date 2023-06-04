c3f7648921786153180f8f78764ebbd83d1a22d1
Pranav Mehta <pranavm.197@gmail.com>
2023-06-04 18:34:28 +0530

    added homepage, learnt a bit more about css tailwind, no major change here

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import {AiOutlineArrowRight} from 'react-icons/ai'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div name='home' className=' h-screen w-full bg-gradient-to-b from-black  to-gray-800'>
+      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row text-white'>
+        <div >
+          <h2>Hey There!</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum modi cumque, sint itaque dolores, repellendus ipsum quod incidunt molestiae corporis non sunt, in tempore quia qui ad? Possimus, harum eaque!</p>
+        </div>
+        <button>
+          Portfolio
+          <span>
+            <AiOutlineArrowRight/>
+          </span>
+        </button>  
+      </div>
+    </div>
+    
   )
 }
 
