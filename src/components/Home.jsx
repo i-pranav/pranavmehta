835aaf356f5df5c4cd6306f447c98efb86a2d53d
Pranav Mehta <pranavm.197@gmail.com>
2023-06-05 23:02:21 +0530

    added more styling... busy day couldn't do much, would be more active from tomorrow

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
   return (
     <div name='home' className=' h-screen w-full bg-gradient-to-b from-black  to-gray-800'>
       <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row text-white'>
-        <div >
-          <h2>Hey There!</h2>
+        <div className='flex flex-col justify-center h-full' >
+          <h2 className=' text-4xl sm:text-7xl font-bold text-white'>Hey There!</h2>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum modi cumque, sint itaque dolores, repellendus ipsum quod incidunt molestiae corporis non sunt, in tempore quia qui ad? Possimus, harum eaque!</p>
         </div>
         <button>
